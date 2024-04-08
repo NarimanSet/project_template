01af27b41dadd18b161a3e546a5cbe3539c5203c
NarimanSet <narimansetmanbetow@gmail.com>
2024-04-08 16:30:25 +0300

    Practice 7 is done

--- app/io/input.py
+++ app/io/input.py
@@ -1,10 +1,23 @@
 def input_text():
-    """Функція для введення тексту з консолі."""
+    """
+    Функція для введення тексту з консолі.
+
+    Returns:
+        str: Введений текст.
+    """
     text = input("Введіть текст: ")
     return text
 
 def read_from_file_builtin(filename):
-    """Функція для зчитування тексту з файлу за допомогою вбудованих можливостей Python."""
+    """
+        Функція для зчитування тексту з файлу за допомогою вбудованих можливостей Python.
+
+        Args:
+            filename (str): Ім'я файлу, з якого буде зчитано текст.
+
+        Returns:
+            str або None: Зчитаний текст або None, якщо файл не було знайдено.
+        """
     try:
         with open(filename, 'r') as file:
             text = file.read()
@@ -14,7 +27,15 @@ def read_from_file_builtin(filename):
         return None
 
 def read_from_file_pandas(filename):
-    """Функція для зчитування тексту з файлу за допомогою бібліотеки pandas."""
+    """
+        Функція для зчитування тексту з файлу за допомогою бібліотеки pandas.
+
+        Args:
+            filename (str): Ім'я файлу, з якого буде зчитано текст.
+
+        Returns:
+            str або None: Зчитаний текст або None, якщо файл не було знайдено.
+        """
     try:
         import pandas as pd
         data = pd.read_csv(filename, header=None)
--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from app.io.input import input_text, read_from_file_pandas, read_from_file_builtin
+from app.io.output import output_text, write_to_file_builtin
+
+
 def main():
     pass
+
+text_input = input_text()
+text_builtin = read_from_file_builtin("data/test2.txt")
+text_pandas = read_from_file_pandas("data/test3.csv")
+
+output_text(text_input)
+output_text(text_builtin)
+output_text(text_pandas)
+
+write_to_file_builtin("data/copy1.txt", text_input)
+write_to_file_builtin("data/copy2.txt", text_builtin)
+write_to_file_builtin("data/copy3.txt", text_pandas)
+
 
 if __name__ == '__main__':
     main()
